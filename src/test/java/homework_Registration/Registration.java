0d65c9119cd0124120ce3b35da14bda8c3fed299
s7678 <s7678@abv.bg>
2023-03-03 23:53:20 +0200

    testng

---
+++ src/test/java/homework_Registration/Registration.java
@@ -0,0 +1,10 @@
+package homework_Registration;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Registration {
+    public static void main(String[] args){
+        WebDriverManager.chromedriver().setup();
+
+    }
+}
